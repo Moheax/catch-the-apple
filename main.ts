752381efdcaf9f2ee1caca2d006f6794d6d60b8a
TheMaksoo <39918032+Moheax@users.noreply.github.com>
2021-09-24 14:44:15 +0200

    main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -5,28 +5,32 @@ input.onButtonPressed(Button.B, function () {
     playerSprite.change(LedSpriteProperty.X, 1)
 })
 let playerSprite: game.LedSprite = null
-let score = 0
+let speed = 500
+let lifes = 3
+basic.showNumber(3)
+basic.showIcon(IconNames.Heart)
 playerSprite = game.createSprite(2, 4)
 let appleSprite = game.createSprite(randint(0, 4), 0)
 appleSprite.turn(Direction.Right, 90)
 appleSprite.set(LedSpriteProperty.Brightness, 50)
 basic.forever(function () {
-	
-})
-basic.forever(function () {
-    basic.pause(500)
+    basic.pause(speed)
     appleSprite.move(1)
     if (appleSprite.isTouching(playerSprite)) {
         appleSprite.set(LedSpriteProperty.X, randint(0, 4))
         appleSprite.set(LedSpriteProperty.Y, 0)
-        score += 1
+        lifes += -1
     } else if (appleSprite.get(LedSpriteProperty.Y) == 4) {
+        basic.pause(100)
         appleSprite.set(LedSpriteProperty.X, randint(0, 4))
         appleSprite.set(LedSpriteProperty.Y, 0)
-        score += -1
+        speed += -10
     }
-    if (score == 10) {
-        basic.showString("W")
-        game.pause()
+    if (lifes == 0) {
+        basic.showIcon(IconNames.Skull)
+        game.gameOver()
     }
+})
+basic.forever(function () {
+	
 })
